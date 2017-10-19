73e93f0b62db126a7203a5d065e6a0ce379bfefe
jossava <jossava@eafit.edu.co>
2017-10-19 00:41:11 -0500

    reto mapReduce modificado

--- mapreduce.py
+++ mapreduce.py
@@ -1,21 +1,28 @@
+# Autor: jossava
 import sys
 
-def map(k,v):
+def map(k,v,c):
 	file = open(sys.argv[1], 'r')
 	auxOutMap = {}
 	outMap = {}
 	for line in file:
 		line = line.split(",")
-		print line
+		#print line
 		second = [int(line[v]),1]
 		if not line[k] in auxOutMap:
-			auxOutMap[line[k]]=second
+			if not c:
+				auxOutMap[line[k]]=second
+			else:
+				auxOutMap[line[k]]=[1,1]
 		else:
-			auxOutMap[line[k]][0]+=second[0]
-			auxOutMap[line[k]][1]+=second[1]
+			if not c:
+				auxOutMap[line[k]][0]+=second[0]
+				auxOutMap[line[k]][1]+=second[1]
+			else:
+				auxOutMap[line[k]][0]+=1
 	for i in auxOutMap:
 		outMap[i]=auxOutMap[i][0]/auxOutMap[i][1]
 	print outMap
 	file.close()
 
-map(1,2)
+map(1,2,False)
